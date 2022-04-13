e297195717cdab831ae2b4158dd9bf42fbc3956a
alexamikaya <alexamikaya@gmail.com>
2022-04-13 10:28:45 +0300

    first program with guava

---
+++ src/main/java/HelloOtus.java
@@ -0,0 +1,14 @@
+import com.google.common.math.IntMath;
+
+import java.math.RoundingMode;
+
+public class HelloOtus {
+    public static void main(String args[]) {
+
+        int a = 121;
+
+        int number = IntMath.sqrt(a, RoundingMode.HALF_EVEN);
+
+        System.out.println("Квадратный корень числа = " + number);
+    }
+}
