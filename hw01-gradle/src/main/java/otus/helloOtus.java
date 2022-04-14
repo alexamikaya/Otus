b0039c6dd019ed3c54b95b891bc9950e65dae2aa
alexamikaya <alexamikaya@gmail.com>
2022-04-14 19:20:56 +0300

    first program with guava

---
+++ hw01-gradle/src/main/java/otus/helloOtus.java
@@ -0,0 +1,18 @@
+package otus;
+
+import com.google.common.math.IntMath;
+
+import java.math.RoundingMode;
+
+public class helloOtus {
+        public static void main(String args[]) {
+
+            int a = 121;
+
+            int number = IntMath.sqrt(a, RoundingMode.HALF_EVEN);
+
+            System.out.println("Result equals " + number);
+        }
+}
+
+
